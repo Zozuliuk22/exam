0365be12cb74824b31d8abfc021cd16c866af1b1
Viktor Zozuliuk <zozulyuk22@gmail.com>
2020-12-23 07:38:04 +0200

    Entered information about numbers

---
+++ division.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -n "Enter first number : " 
+read first_number  
+      
+echo -n "Enter second number : "
+read second_number
+
+echo -n "Enter number round : "
+read round_number
