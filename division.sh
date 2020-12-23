d897f9f02ad0c419378d3144cb04f5adde008fdf
Viktor Zozuliuk <zozulyuk22@gmail.com>
2020-12-23 07:42:41 +0200

    Added zero division check

--- division.sh
+++ division.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-echo -n "Enter first number : " 
-read first_number  
+while true; do #ввод чисел пока происходит ошибка деления на ноль
+	echo -n "Enter first number : " 
+	read first_number  
       
-echo -n "Enter second number : "
-read second_number
+	echo -n "Enter second number : "
+	read second_number
 
-echo -n "Enter number round : "
-read round_number
+	echo -n "Enter number round : "
+	read round_number
 
-rezult=$(echo "scale=$round_number; $first_number/$second_number" | bc)
+	if [[ $second_number = 0 ]]; then  #проверка деления на ноль
+  	  echo "Error! Divide by zero!"
+	else 
+          rezult=$(echo "scale=$round_number; $first_number/$second_number" | bc)
+  	  break
+	fi
+done
 
 echo "Rezult = $rezult"
